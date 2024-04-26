2a0b5ff131c83f13b1ff75a64b182b1035a43778
Ygor Queiroz <ygor.silva@eaportal.org>
2024-04-26 10:45:36 +0000

    terminei css

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 let botoes = document.querySelectorAll(".botoes button")
-
 botoes.forEach(botao => {
     botao.addEventListener("click", guardarNota)
 })
 
 function guardarNota(evento) {
-    let nota= evento.target.innerText
+    let nota = evento.target.innerText
 
     localStorage.setItem("nota", nota)
 }
